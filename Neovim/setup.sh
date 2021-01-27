813f3ece64acdf668caf04ba6da5976cdc3ee2f0
Mahanth Mohan <mohan.mahanth@gmail.com>
2021-01-27 13:37:42 -0800

    added some config

--- Neovim/setup.sh
+++ Neovim/setup.sh
@@ -1,4 +1,10 @@
 echo "** Starting Neovim Setup **"
 set -e
+# Run these commands one at a time
 nvim +PlugInstall +PlugUpdate +PlugUpgrade +CocInstall +CocUpdate
+exit
+nvim +CocInstall coc-clangd coc-java coc-tsserver coc-python coc-go coc-flutter coc-jedi coc-html coc-css coc-vetur coc-snippets coc-vimlsp
+exit
+nvim +CocInstall +CocUpdate
+exit
 echo "** Setup Finished **" 
