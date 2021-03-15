658b555e7908dad3451b4e1f7be680948179e55a
Mahanth <mohan.mahanth@gmail.com>
2021-03-15 10:14:54 -0700

    final changes

--- Neovim/setup.sh
+++ Neovim/setup.sh
@@ -3,7 +3,7 @@ set -e
 # Run these commands one at a time
 nvim +PlugInstall +PlugUpdate +PlugUpgrade +CocInstall +CocUpdate
 exit
-nvim +CocInstall coc-clangd coc-java coc-tsserver coc-python coc-go coc-flutter coc-jedi coc-html coc-css coc-vetur coc-snippets coc-vimlsp
+nvim +CocInstall coc-clangd coc-java coc-tsserver coc-python coc-go coc-flutter coc-jedi coc-json coc-html coc-css coc-vetur coc-snippets coc-vimlsp
 exit
 nvim +CocInstall +CocUpdate
 exit
