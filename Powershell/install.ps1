970c441e3c7ed0fa05fe412d903b317b7f469eb5
Mahanth <mohan.mahanth@gmail.com>
2021-05-09 14:29:18 -0700

    added changes

--- Powershell/install.ps1
+++ Powershell/install.ps1
@@ -1,6 +1,6 @@
 Write-Host "*** Installing all modules ***"
 Write-Host "Step 1: Installing posh-git & oh-my-posh"
-Install-Module posh-git oh-my-posh -Scope CurrentUser
+Install-Module posh-git, oh-my-posh -Scope CurrentUser
 Write-Host "Step 2: Installing PSReadLine"
 Install-Module PSReadLine -Scope CurrentUser
 Write-Host "*** Completed Installation ***"
