a1e61d134dc1f414cc3fe63fe15948ee1595466a
Mahanth <mohan.mahanth@gmail.com>
2021-11-11 13:36:30 -0800

    added setup scripts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # .config
-A place to store all my terminal setup, config folders, and files
+A place to store all my terminal setup, config folders, and files. Contains setup for the Alacritty terminal emulator and vim, for useful features such as plugins and code completion. 
+
+## Setup
+shell ```
+git clone https://github.com/MahanthMohan/.config.git
+cd .config
+./config.sh
+```
---
+++ config.sh
@@ -0,0 +1,21 @@
+echo "--- Setting up environment ---"
+cp vim/.vimrc ~
+
+zshExists=$(cat /etc/shells | grep -c "zsh")
+
+if [ $zshExists -eq 0 ]; then
+		echo "Installing zsh"
+		sudo apt update -y && sudo apt install -y zsh
+else
+		echo "zsh already installed"
+		sudo apt update -y
+fi
+
+echo "Installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp zsh/.zshrc ~
+cp -r alacritty ~/.config
+echo "Installing vim plugins and setup"
+cp ./vim/script.sh
+echo "--- Setup complete ---"
